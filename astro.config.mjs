cd34f137dcc4ac7b2a1d42fe998bf5e096c51927
Kollu Charan <kollu.c@talview.com>
2025-08-07 18:46:39 +0530

    some-fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,30 +5,32 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { modifiedTime, readingTime } from "./src/lib/utils/remarks.mjs";
 import { SITE } from "./src/lib/config";
-import keystatic from "@keystatic/astro";
-import react from "@astrojs/react";
-import { loadEnv } from "vite";
+import sanityIntegration from '@sanity/astro';
+  import netlify from "@astrojs/netlify";
+import react from '@astrojs/react';
 import pagefind from "astro-pagefind";
-
-const { RUN_KEYSTATIC } = loadEnv(import.meta.env.MODE, process.cwd(), "");
-
-const integrations = [mdx(), sitemap(), pagefind()];
-
-if (RUN_KEYSTATIC === "true") {
-  integrations.push(react());
-  integrations.push(keystatic());
-}
-
-// https://astro.build/config
 export default defineConfig({
   site: SITE.url,
   base: SITE.basePath,
   markdown: {
     remarkPlugins: [readingTime, modifiedTime],
   },
-
   image: {},
-  integrations,
+//  output: 'server', 
+//   adapter: netlify({}),
+integrations: [
+    mdx(),
+    sitemap(),
+    pagefind(),
+    react(), 
+    sanityIntegration({
+      projectId: '0fl11mre',   
+      dataset: 'production',         
+      useCdn: false,
+      apiVersion: '2023-01-01',
+      studioBasePath: '/studio',      
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
