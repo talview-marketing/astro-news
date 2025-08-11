20415bc8962edaeeab573221b6fd84ce6cc6358b
Kollu Charan <kollu.c@talview.com>
2025-08-11 10:17:01 +0530

    removed files

--- src/lib/utils/remarks.mjs
+++ src/lib/utils/remarks.mjs
@@ -19,8 +19,7 @@ export function readingTime() {
   return (tree, { data }) => {
     const textOnPage = ConvertToString(tree);
     const readingTime = getReadingTime(textOnPage, { wordsPerMinute: 180 });
-    // readingTime.text will give us minutes read as a friendly string,
-    // i.e. "3 min read"
+
     data.astro.frontmatter.minutesRead = readingTime.text;
   };
 }
